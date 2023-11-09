5441fefb076cc084462a7a48c3af71e6cc0a1711
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-11-10 01:07:47 +0700

    create signin step1

---
+++ signin-step3.php
@@ -0,0 +1,66 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <div class="sign-in-page-wrapper background-common">
+    <div class="page-content-wrapper-common">
+      <a href="#" target="_self" title="" class="header-logo">
+        <img src="./images/logo-bitu-kids.png" class="header-logo-image" alt="">
+      </a>
+      <div class="container main-page-content">
+        <div class="main-box-wrapper login-step3-block-wrapper">
+          <div class="common-badge-wrapper">
+            <div class="back-background"></div>
+            <div class="front-background">
+              <span class="text-label">Đăng nhập</span>
+            </div>
+          </div>
+          <div class="step2-content-wrapper">
+            <div class="common-form-wrapper">
+              <div class="form-content-wrapper">
+                <div class="step3-content-detail">
+                  <div class="top-block-wrapper">
+                    <div class="logo-wrapper">
+                      <img src="./images/icon-logo-1.png" class="icon-logo-1" alt="">
+                    </div>
+
+                    <div class="arrow-wrapper">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+                        <g clip-path="url(#clip0_2903_20126)">
+                          <path d="M11.666 11.666L19.9993 19.9993L11.666 28.3327" stroke="#FF6D03" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path d="M21.666 11.666L29.9993 19.9993L21.666 28.3327" stroke="#FF6D03" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                          <clipPath id="clip0_2903_20126">
+                            <rect width="40" height="40" fill="white"/>
+                          </clipPath>
+                        </defs>
+                      </svg>
+                    </div>
+
+                     <div class="logo-wrapper">
+                      <img src="./images/icon-logo-2.png" class="icon-logo-2" alt="">
+                     </div>
+                  </div>
+
+                  <p class="description">
+                    Tài khoản hiện đã có trên Bitu, bạn có chuyển đổi tài khoản sang thành tài khoản Kids?
+                  </p>
+
+                  <button class="common-btn btn-covert-account">
+                    <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                    <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                    <span class="text-btn">Chuyển đổi</span>
+                  </button>
+
+                  <p class="note">Lưu ý:  Tài khoản sau khi chuyển đổi sẽ được tạo sẵn 1 hồ sơ phụ huynh và 1 hồ sơ cho bé: bao gồm đẩy đủ thông tin về buổi học, lịch sử học của tài khoản được giữ nguyên.</p>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ test.php
@@ -0,0 +1,13 @@
+<button class="common-btn btn-create-account">
+  <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+  <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+  <span class="text-btn">Tạo tài khoản mới</span>
+</button>
+
+
+<div class="common-badge-wrapper">
+  <div class="back-background"></div>
+  <div class="front-background">
+    <span class="text-label">Đăng nhập</span>
+  </div>
+</div>
