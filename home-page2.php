0ad25df284a7593e07171bb5c7d708773507cb39
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-11-11 19:51:51 +0700

    Fix: css

---
+++ home-page2.php
@@ -0,0 +1,25 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <div class="background-common">
+    <div class="page-content-wrapper-common home-page-wrapper">
+      <!-- IMPORT  HEADER -->
+      <?php include "templates/homeHeader.php"; ?>
+      <div class="home-page-content-wrapper">
+        <div class="container">
+          nguyen dang khoa - hoc vien cong nghe buu chinh vien thong
+        </div>
+
+        <!-- button control next to road map -->
+        <button class="common-control-roadmap btn-prev">
+          <span class="btn-text">Kiểm tra &
+            học thử</span>
+          <img src="./images/icon-btn-next.png" class="icon-arrow" alt="">
+        </button>
+        <!-- button control next to road map -->
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
