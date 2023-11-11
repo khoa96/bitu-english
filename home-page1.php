fc314c37486bfbae91925bcce4746f3e91277921
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-11-11 19:09:37 +0700

    fix css

--- home-page1.php
+++ home-page1.php
@@ -107,9 +107,9 @@
                 </ul>
             </div>
             <div class="right-content">
-              <div class="common-topic-item normal-topic">
+              <div class="common-topic-item big-topic">
                 <div class="topic-header">
-                  <img src="./images/topic-image-1.png" class="topic-image" alt="">
+                  <img src="./images/topic-image-2.png" class="topic-image" alt="">
                 </div>
                 <div class="topic-content">
                   <h3 class="topic-title">
@@ -121,15 +121,15 @@
                         <img src="./images/icon-outline.png" class="action-icon" alt="">
                         <span class="label">Outline</span>
                       </div>
-                     <div class="action-block">
+                     <!-- <div class="action-block">
                         <img src="./images/icon-listen.png" class="action-icon" alt="">
                         <span class="label">Listen</span>
-                      </div>
+                      </div> -->
                     </div>
                     <button class="common-btn btn-join-topic">
                       <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
                       <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
-                      <span class="text-btn">Join</span>
+                      <span class="text-btn">Đặt lịch</span>
                     </button>
                   </div>
                 </div>
