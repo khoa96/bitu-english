41c098f6c6f54d3e169df289a8ccd3b6925a7b6a
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-11-12 11:51:46 +0700

    create home page step 5

---
+++ home-page3.php
@@ -0,0 +1,275 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <div class="background-common">
+    <div class="page-content-wrapper-common home-page-wrapper">
+      <!-- IMPORT  HEADER -->
+      <?php include "templates/homeHeader.php"; ?>
+      <div class="home-page-content-wrapper roadmap-home-page-wrapper">
+        <div class="page-roadmap-content-wrapper">
+          <div class="row-item">
+            <!-- BẮT ĐẦU 3 LEVEL HÀNG TRÊN CÙNG -->
+            <div class="row-content">
+              <div class="level-item disabled">
+                <span class="name-level">Advanced</span>
+                <div class="image-level-wrapper">
+                  <img src="./images/icon-lock.png" class="icon-lock" alt="">
+                  <img src="./images/icon-roadmap-7.png" class="image-level" alt="">
+                </div>
+
+                <ul class="list-star">
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                </ul>
+              </div>
+              <div class="level-item disabled">
+                <span class="name-level">Upper Flyer</span>
+                <div class="image-level-wrapper">
+                  <img src="./images/icon-lock.png" class="icon-lock" alt="">
+                  <img src="./images/icon-roadmap-6.png" class="image-level" alt="">
+                </div>
+
+                <ul class="list-star">
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                </ul>
+              </div>
+              <div class="level-item disabled">
+                <span class="name-level">Flyer</span>
+                <div class="image-level-wrapper">
+                  <img src="./images/icon-lock.png" class="icon-lock" alt="">
+                  <img src="./images/icon-roadmap-5.png" class="image-level" alt="">
+                </div>
+
+                <ul class="list-star">
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                </ul>
+              </div>
+            </div>
+            <!-- BẮT ĐẦU 3 LEVEL HÀNG TRÊN CÙNG -->
+          </div>
+
+          <!-- BẮT ĐẦU 1 LEVEL HÀNG GIỮA-->
+          <div class="row-item">
+            <div class="level-item disabled">
+              <span class="name-level">Upper Mover</span>
+              <div class="image-level-wrapper">
+                <img src="./images/icon-lock.png" class="icon-lock" alt="">
+                <img src="./images/icon-roadmap-4.png" class="image-level" alt="">
+              </div>
+
+              <ul class="list-star">
+                <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+              </ul>
+
+              <!-- HIỂN THỊ NỘI DUNG TOOLTIP KHI HOVER -->
+              <div class="tooltip-block-wrapper">
+                <h3 class="tooltip-title">Mục tiêu Upper Starter</h3>
+                <ul class="list-target">
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                  </li>
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about</p>
+                  </li>
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                  </li>
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about</p>
+                  </li>
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                  </li>
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about</p>
+                  </li>
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                  </li>
+                  <li class="target-item">
+                    <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                    <p class="txt-target">Can answer simple questions about</p>
+                  </li>
+                </ul>
+              </div>
+              <!-- HIỂN THỊ NỘI DUNG TOOLTIP KHI HOVER -->
+            </div>
+          </div>
+          <!-- BẮT ĐẦU 1 LEVEL HÀNG GIỮA-->
+
+
+          <!-- BẮT ĐẦU 3 LEVEL HÀNG CUỐI CÙNG -->
+          <div class="row-item">
+            <div class="row-content">
+              <div class="level-item disabled">
+                <span class="name-level">Starter</span>
+                <div class="image-level-wrapper">
+                  <img src="./images/icon-lock.png" class="icon-lock" alt="">
+                  <img src="./images/icon-roadmap-1.png" class="image-level" alt="">
+                </div>
+
+                <ul class="list-star">
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                </ul>
+
+                <!-- HIỂN THỊ NỘI DUNG TOOLTIP KHI HOVER -->
+                <div class="tooltip-block-wrapper">
+                  <h3 class="tooltip-title">Mục tiêu Upper Starter</h3>
+                  <ul class="list-target">
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                  </ul>
+                </div>
+                <!-- HIỂN THỊ NỘI DUNG TOOLTIP KHI HOVER -->
+              </div>
+              <div class="level-item disabled">
+                <span class="name-level">Upper Starter</span>
+                <div class="image-level-wrapper">
+                  <img src="./images/icon-lock.png" class="icon-lock" alt="">
+                  <img src="./images/icon-roadmap-2.png" class="image-level" alt="">
+                </div>
+
+                <ul class="list-star">
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                </ul>
+
+                <!-- HIỂN THỊ NỘI DUNG TOOLTIP KHI HOVER -->
+                <div class="tooltip-block-wrapper">
+                  <h3 class="tooltip-title">Mục tiêu Upper Starter</h3>
+                  <ul class="list-target">
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about habits and routines.</p>
+                    </li>
+                    <li class="target-item">
+                      <img src="./images/icon-check-2.png" class="icon-check" alt="">
+                      <p class="txt-target">Can answer simple questions about</p>
+                    </li>
+                  </ul>
+                </div>
+                <!-- HIỂN THỊ NỘI DUNG TOOLTIP KHI HOVER -->
+              </div>
+              <div class="level-item disabled">
+                <span class="name-level">Mover</span>
+                <div class="image-level-wrapper">
+                  <img src="./images/icon-lock.png" class="icon-lock" alt="">
+                  <img src="./images/icon-roadmap-3.png" class="image-level" alt="">
+                </div>
+
+                <ul class="list-star">
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                  <li class="star-item"><img src="./images/icon-disable-star.png" alt="" class="star-image"></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- BẮT ĐẦU 3 LEVEL HÀNG CUỐI CÙNG -->
+        </div>
+
+        <!-- button control next to road map -->
+        <button class="common-control-roadmap btn-prev">
+          <span class="btn-text">Kiểm tra &
+            học thử</span>
+          <img src="./images/icon-btn-next.png" class="icon-arrow" alt="">
+        </button>
+        <!-- button control next to road map -->
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
--- test.php
+++ test.php
@@ -11,3 +11,32 @@
     <span class="text-label">Đăng nhập</span>
   </div>
 </div>
+
+
+<div class="common-topic-item big-topic">
+  <div class="topic-header">
+    <img src="./images/topic-image-2.png" class="topic-image" alt="">
+  </div>
+  <div class="topic-content">
+    <h3 class="topic-title">
+      Do you do anything special with your family on holidays
+    </h3>
+    <div class="topic-footer">
+      <div class="left-content">
+        <div class="action-block">
+          <img src="./images/icon-outline.png" class="action-icon" alt="">
+          <span class="label">Outline</span>
+        </div>
+        <!-- <div class="action-block">
+                        <img src="./images/icon-listen.png" class="action-icon" alt="">
+                        <span class="label">Listen</span>
+                      </div> -->
+      </div>
+      <button class="common-btn btn-join-topic">
+        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+        <span class="text-btn">Đặt lịch</span>
+      </button>
+    </div>
+  </div>
+</div>
