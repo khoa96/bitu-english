21722a78d78085ca69b07d0aff35e485346c28f6
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-11-23 13:23:54 +0700

    fix html

---
+++ home-page41.php
@@ -0,0 +1,312 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <div class="home-page-wrapper home-page-roadmap-topic">
+    <!-- IMPORT  HEADER -->
+    <div class="home-page-content-mask">
+      <?php include "templates/homeHeader4.php"; ?>
+      <div class="home-page-roadmap-topic-wrapper">
+        <div class="container home-page-roadmap-topic-content">
+          <div class="list-roadmap-of-topic">
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topc-1.png" class="topic-image" alt="">
+                    <span class="label-learn-again">Ôn tập</span>
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Do you do anything special with your family on holidays
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-learn-again">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Học lại</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-1.png" class="bottom-image-1" alt="">
+              </div>
+            </div>
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topic-2.png" class="topic-image" alt="">
+                    <!-- <span class="label-learn-again">Ôn tập</span> -->
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Talk about your family
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-join-topic">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Join</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-2.png" class="bottom-image-2" alt="">
+              </div>
+            </div>
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topic-3.png" class="topic-image" alt="">
+                    <!-- <span class="label-learn-again">Ôn tập</span> -->
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Do you do anything special with your family on holidays
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-scheduled">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Đã đặt lịch</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-1.png" class="bottom-image-1" alt="">
+              </div>
+            </div>
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topic-4.png" class="topic-image" alt="">
+                    <!-- <span class="label-learn-again">Ôn tập</span> -->
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Do you do anything special with your family on holidays
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-scheduled">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Đã đặt lịch</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-2.png" class="bottom-image-2" alt="">
+              </div>
+            </div>
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topc-1.png" class="topic-image" alt="">
+                    <span class="label-learn-again">Ôn tập</span>
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Do you do anything special with your family on holidays
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-learn-again">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Học lại</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-1.png" class="bottom-image-1" alt="">
+              </div>
+            </div>
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topic-2.png" class="topic-image" alt="">
+                    <!-- <span class="label-learn-again">Ôn tập</span> -->
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Talk about your family
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-join-topic">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Join</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-2.png" class="bottom-image-2" alt="">
+              </div>
+            </div>
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topic-3.png" class="topic-image" alt="">
+                    <!-- <span class="label-learn-again">Ôn tập</span> -->
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Do you do anything special with your family on holidays
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-scheduled">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Đã đặt lịch</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-1.png" class="bottom-image-1" alt="">
+              </div>
+            </div>
+            <div class="roadmap-of-topic-item">
+              <div class="topic-wrapper">
+                <div class="common-topic-item small-topic">
+                  <div class="topic-header">
+                    <img src="./images/topic-4.png" class="topic-image" alt="">
+                    <!-- <span class="label-learn-again">Ôn tập</span> -->
+                  </div>
+                  <div class="topic-content">
+                    <h3 class="topic-title">
+                      Do you do anything special with your family on holidays
+                    </h3>
+                    <div class="topic-footer">
+                      <div class="left-content">
+                        <div class="action-block">
+                          <img src="./images/icon-outline.png" class="action-icon" alt="">
+                          <span class="label">Outline</span>
+                        </div>
+                      </div>
+                      <button class="common-btn btn-scheduled">
+                        <img src="./images/icon-btn-1.png" class="icon-btn-1" alt="">
+                        <img src="./images/icon-btn-2.png" class="icon-btn-2" alt="">
+                        <span class="text-btn">Đã đặt lịch</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="image-bottom-wrapper">
+                <img src="./images/bottom-image-2.png" class="bottom-image-2" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <?php include "templates/footer.php"; ?>
+  <script src="js/collapse.js"></script>
+  <script>
+    $(function () {
+      // PC
+      $(
+        ".list-roadmap-of-topic"
+      ).slick({
+        speed: 600,
+        autoplaySpeed: 6000,
+        autoplay: false,
+        infinite: true,
+        swipe: true,
+        fade: false,
+        dots: false,
+        arrows: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        adaptiveHeight: true,
+        slidesToScroll: 3,
+        responsive: [{
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        ],
+      });
+    });
+  </script>
+  <script>
+    const listSlider = $('.roadmap-of-topic-item')
+    listSlider.each(function (index) {
+      if (index % 2 !== 0) {
+        $(this).css('top', '60px')
+        $(this).next().css('top', '-16px')
+      }
+    })
+  </script>
+</body>
+
+</html>
